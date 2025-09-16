bb2d11ade61639e7ec016bd15a78a05f43a9d39d
hanbi10 <jeonghanbi10@dgsw.hs.kr>
2025-09-16 17:56:56 +0900

    .

---
+++ src/test/java/com/example/springjwt2/SpringJwt2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springjwt2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJwt2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
